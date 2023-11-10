7e6abcabc73ced594fd5792a4ae8071d56ffba48
Julio arcos <julio.arcos.salazar@gmail.com>
2023-11-10 17:47:22 -0600

    Anexo dependencias de Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    // Dependencias de Firebase
+    id("com.google.gms.google-services")
 }
 // Prueba
 android {
@@ -72,6 +75,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Dependencias de Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
 
 
     androidTestImplementation("androidx.navigation:navigation-testing:2.7.4")
