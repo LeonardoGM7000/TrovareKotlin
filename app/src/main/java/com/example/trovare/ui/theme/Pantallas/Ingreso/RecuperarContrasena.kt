283a5a2651fdc59da1a4c274e48645df43ebd609
Julio arcos <julio.arcos.salazar@gmail.com>
2023-11-28 20:12:14 -0600

    Lógica Recuperación de Contraseña (Detalles con la validación de contraseña de Firebase Auth)

--- app/src/main/java/com/example/trovare/ui/theme/Pantallas/Ingreso/RecuperarContrasena.kt
+++ app/src/main/java/com/example/trovare/ui/theme/Pantallas/Ingreso/RecuperarContrasena.kt
@@ -1,5 +1,11 @@
 package com.example.trovare.ui.theme.Pantallas.Ingreso
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
+import android.util.Patterns
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,17 +24,24 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -41,6 +54,10 @@ import com.example.trovare.ui.theme.Recursos.BarraSuperior
 import com.example.trovare.ui.theme.Trv1
 import com.example.trovare.ui.theme.Trv6
 import com.example.trovare.ui.theme.Trv8
+import com.google.firebase.auth.ActionCodeSettings
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -48,6 +65,16 @@ fun RecuperarContrasena(
     modifier: Modifier = Modifier,
     navController: NavController
 ){
+
+    // Definimos las variables
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    var isErrorC by rememberSaveable { mutableStateOf(false) }
+    val context = LocalContext.current
+    val auth = FirebaseAuth.getInstance()
+    var guardadoExitoso by remember { mutableStateOf(false) }
+
+
     var textoCorreo by rememberSaveable(stateSaver = TextFieldValue.Saver) {
         mutableStateOf(TextFieldValue("", TextRange(0, 7)))
     }
@@ -55,9 +82,23 @@ fun RecuperarContrasena(
     val keyboardOptionsCorreo: KeyboardOptions =
         KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email, imeAction = ImeAction.Done)
 
+    LaunchedEffect(guardadoExitoso){
+
+        if(guardadoExitoso) {
+            delay(1000)
+            guardadoExitoso = false
+            navController.popBackStack()
+        }
+    }
+
     Scaffold(
         topBar = {
             BarraSuperior(navController = navController)
+        },
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState
+            )
         },
     ) {
         Surface(
@@ -90,10 +131,13 @@ fun RecuperarContrasena(
                         color = Color.White,
                         style = MaterialTheme.typography.bodySmall
                     )
+
+                    // Correo ----------------------------------------------------------------------
                     OutlinedTextField(
                         modifier = modifier
                             .fillMaxWidth()
                             .padding(start = 25.dp, end = 25.dp, bottom = 15.dp),
+                        isError = isErrorC,
                         value = textoCorreo,
                         onValueChange = { textoCorreo = it },
                         leadingIcon = { Icon(imageVector = Icons.Rounded.Mail, contentDescription = "") },
@@ -129,7 +173,68 @@ fun RecuperarContrasena(
                             .fillMaxWidth()
                             .padding(start = 25.dp, end = 25.dp, bottom = 10.dp),
                         onClick = {
-                                  navController.navigate(Pantalla.TokenRecuperarContrasena.ruta)
+
+                            // Realizamos las validadciones
+                            if(textoCorreo.text.isBlank()){
+
+                                Log.i("error_Recuperar", "campos imcompletos")
+                                scope.launch {
+                                    snackbarHostState.showSnackbar(
+                                        message = "Campos obligatorios no completados",
+                                        duration = SnackbarDuration.Short
+                                    )
+                                }
+                            } else if(!Patterns.EMAIL_ADDRESS.matcher(textoCorreo.text).matches()){
+                                Log.i("error correo", textoCorreo.text)
+                                scope.launch {
+                                    isErrorC = true
+                                    snackbarHostState.showSnackbar(
+                                        message = "Correo inválido",
+                                        duration = SnackbarDuration.Short
+                                    )
+                                }
+                            } else if(!isNetworkAvailable(context)){
+                                Log.i("error conexión", "No hay conexión a internet")
+                                scope.launch {
+                                    snackbarHostState.showSnackbar(
+                                        message = "Error de conexión",
+                                        duration = SnackbarDuration.Short
+                                    )
+                                }
+
+                            } else{
+
+                                auth.sendPasswordResetEmail(textoCorreo.text)
+                                    .addOnCompleteListener{task ->
+
+                                        if(task.isSuccessful){
+
+                                            Log.d("Recuperar_Pass", "Correo enviado")
+                                            isErrorC = false
+                                            scope.launch {
+                                                snackbarHostState.showSnackbar(
+                                                    message = "Verifica tu correo para reestablecer tu contraseña",
+                                                    duration = SnackbarDuration.Short
+                                                )
+                                            }
+
+                                            guardadoExitoso = true
+
+                                        }else{
+
+                                            Log.d("Recuperar_Pass", "Correo no enviado")
+                                            scope.launch {
+                                                snackbarHostState.showSnackbar(
+                                                    message = "Ingrese un correo válido",
+                                                    duration = SnackbarDuration.Short
+                                                )
+                                            }
+                                        }
+                                    }
+                            }
+
+
+
                                   },
                         colors = ButtonDefaults.buttonColors(
                             containerColor = Trv6,
@@ -142,4 +247,19 @@ fun RecuperarContrasena(
             }
         }
     }
+}
+
+
+//Funciones auxiliares
+@SuppressLint("ServiceCast")
+private fun isNetworkAvailable(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    val network = connectivityManager.activeNetwork ?: return false
+    val capabilities =
+        connectivityManager.getNetworkCapabilities(network) ?: return false
+
+    return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
 }
